f783b0333f9b5c53f83c501b79576873c6485ab7
honzikv <honzikv@students.zcu.cz>
2020-10-18 17:26:01 +0200

    test with half size mnist

--- nengo-examples/src/DataParser.py
+++ nengo-examples/src/DataParser.py
@@ -1,14 +1,51 @@
+# Wrapper for parsing data from p300 directory
+import logging
+import os
+from os import listdir
+
+import mne
+from mne.io.brainvision.brainvision import RawBrainVision
+
+logger = logging.getLogger(__name__)
+
+
 class DataParser:
+    _required_file_extensions = ['.eeg', '.vhdr', '.vmrk']
 
-    def __init__(self, eeg_file_path: str, vhdr_path: str, vmrk_path: str):
-        self.eeg_file_path = eeg_file_path
-        self.vhdr_path = vhdr_path
-        self.vmrk_path = vmrk_path
+    def __init__(self, dataset_path=os.path.join('..', 'datasets', 'p300')):
+        self.dataset_path = dataset_path
 
-    def parse(self):
+    def parse_dataset(self):
         pass
 
+    @staticmethod
+    def visualize_data(raw_brain_vision: RawBrainVision):
+        events = mne.find_events(raw_brain_vision)
 
-import tensorflow as tf
-print(tf.__version__)
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+    @staticmethod
+    def __parse_experiment__(data_dir_path: str):
+        vhdr_file = None
+
+        for file in listdir(data_dir_path):
+            extension = os.path.splitext(file)[1]
+            if extension == '.vhdr':
+                vhdr_file = os.path.join(data_dir_path, file)
+
+        if vhdr_file is None:
+            logger.log(logging.ERROR,
+                       'VHDR file not found, skipping this experiment, it is probably corrupt or missing data')
+            return
+
+        raw_brain_vision = mne.io.read_raw_brainvision(
+            vhdr_fname=vhdr_file,
+            eog=['Fz', 'Cz', 'Pz', 'EOG']
+        )
+        raw_brain_vision.plot()
+        return raw_brain_vision
+
+
+data_parser = DataParser()
+data_parser.__parse_experiment__(
+    os.path.join(data_parser.dataset_path, 'Experiment_341_P3_Numbers', 'Data')
+)
---
+++ nengo-examples/src/Experiment.py
@@ -0,0 +1,6 @@
+class Experiment:
+
+    def __init__(self, eeg_file, vhdr_file, vmrk_file):
+        self.eeg_file = eeg_file
+        self.vhdr_file = vhdr_file
+        self.vmrk_file = vmrk_file
