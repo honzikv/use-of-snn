3c969b41b58868b786b203e5bbf235d196fd1671
honzikv <honzikv@students.zcu.cz>
2020-10-19 14:48:54 +0200

    Dataset preprocessing

--- nengo-examples/src/DatasetPreprocessor.py
+++ nengo-examples/src/DatasetPreprocessor.py
@@ -15,50 +15,74 @@ logger = logging.getLogger(__name__)
 
 class DatasetPreprocessor:
 
-    def __init__(self, dataset_path=os.path.join('..', 'datasets', 'p300')):
+    def __init__(self, dataset_path=os.path.join('..', 'datasets', 'p300'), log_info=False,
+                 export=True, output_path=os.path.join('..', 'datasets', 'p300'), output_filename='p300_dataset'):
         self.dataset_path = dataset_path
+        self.export = export
+        self.output_path = output_path
+        self.log_info = log_info
+        self.output_filename = output_filename
 
-    def parse_dataset(self, test_data_percentage: float = .25, seed=0) \
-            -> Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+        if not log_info:
+            mne.set_log_level(verbose='ERROR')
+
+    def create_dataset(self, test_data_percentage: float = .25, seed=0):
         """
-        :param seed: seed for shuffling dataset
+        :param seed: seed for dataset shuffle to provide consistent results
         :param test_data_percentage: percentage of training data instead used as test data - from (0, 0.5)
         :return: tuple (train_x, train_y, test_x, test_y)
         """
 
         if 0 > test_data_percentage > .5:
-            test_data_percentage = .5
+            test_data_percentage = .25
 
         all_epochs = []
         for folder in listdir(self.dataset_path):
-            data_dir_path = os.path.join(self.dataset_path, folder, 'Data')
+            path = os.path.join(self.dataset_path, folder)
+            if os.path.isfile(path):
+                continue
+
+            data_dir_path = os.path.join(path, 'Data')
             experiment_epochs = DatasetPreprocessor.__parse_experiment__(data_dir_path)
             all_epochs.append(experiment_epochs)
 
         random.seed(seed)
         random.shuffle(all_epochs)
+        logger.info('concatenating loaded data')
         features, labels = self.__concatenate_data__(all_epochs)
 
-        test_data_index = features.shape[0] * (1 - test_data_percentage)
+        logger.info('splitting to train and test data')
+        test_data_index = int(features.shape[0] * (1 - test_data_percentage))
         train_x, train_y = features[:test_data_index], labels[:test_data_index]
-        test_x, test_y = features[test_data_index:], labels[:test_data_index]
+        test_x, test_y = features[test_data_index:], labels[test_data_index:]
 
-        return (train_x, train_y), (test_x, test_y)
+        if self.export:
+            data = [train_x, train_y, test_x, test_y]
+            save_filename = os.path.join(self.output_path, self.output_filename)
+            np.savez_compressed(file= save_filename, *data)
+            logger.info('Output dataset saved to:',save_filename)
+
+        return train_x, train_y, test_x, test_y
 
     @staticmethod
-    def __concatenate_data__(epochs: List[mne.Epochs]) -> Tuple[np.ndarray, np.ndarray]:
+    def __concatenate_data__(epochs: List[mne.Epochs]):
+        if len(epochs) <= 1:
+            logger.error('Error while parsing epochs')
+
         epochs[0].load_data()
-        features = np.ndarray(epochs[0].get_data())
-        labels = np.ndarray(epochs[0].events)
+        features = epochs[0].get_data()
+        a = epochs[0].events
+        labels = list(epochs[0].events[:, 2])  # only class is relevant
 
         for i in range(1, len(epochs)):
             epoch = epochs[i]
             epoch.load_data()
 
-            np.row_stack(features, epoch.get_data())
-            np.row_stack(labels, epoch.events)
+            features = np.row_stack((features, epoch.get_data()))
+            epoch_events = list(epoch.events[:, 2])
+            labels += epoch_events
 
-        return features, labels
+        return features, np.array(labels)
 
     @staticmethod
     def __parse_experiment__(data_dir_path: str):
@@ -86,4 +110,8 @@ class DatasetPreprocessor:
 
 
 data_parser = DatasetPreprocessor()
-data_parser.parse_dataset(.23)
+train_x, train_y, test_x, test_y = data_parser.create_dataset(.23, 1)
+print(train_x.shape)
+print(train_y.shape)
+print(test_x.shape)
+print(test_y.shape)
