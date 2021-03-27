68312f366e26808fd87c1f4fdad69a5c8cac4777
honzikv <honzikv@students.zcu.cz>
2021-03-27 16:03:28 +0100

    1st experiment additional metrics

--- spindle-detection/MODA_GC-3.0/lib/saveEEGVector.m
+++
@@ -1,5 +0,0 @@
-% Save all files related to the eegvector
-function saveEEGVector( pathOutput, iphase, EEGvector, EEGvectorFileName)
-    save(sprintf('%s%s_p%i.mat', pathOutput, EEGvectorFileName, iphase),'EEGvector');
-end
-
